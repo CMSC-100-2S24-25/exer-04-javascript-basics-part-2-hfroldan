8602842bdf6e956da51b5bda265b81baff59b85b
hfroldan <hfroldan@up.edu.ph>
2025-02-26 12:07:51 +0800

    feat: Add generateUniqueID(), addAccount(), and helper isStringNull

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ import isEmail from 'validator/lib/isEmail.js';
 import { appendFileSync } from 'node:fs';
 
 function generateUniqueID(firstName, lastName) {
-    return null
+    return `${firstName[0].toLowerCase()}${lastName.toLowerCase()}${uuidv4().replace(/-/g,'').substring(0, 8)}`
+}
+
+function isStringNull(str) {
+    return str === null ? true : false
 }
 
 function addAccount(accInfo) {
-    return null
+    if (!Array.isArray(accInfo) || accInfo.length != 4) return false;
+
+    let firstName = accInfo[0], 
+        lastName = accInfo[1], 
+        email = accInfo[2],
+        age = accInfo[3]
+
+    if (isStringNull(firstName) || 
+        isStringNull(lastName) || 
+        isStringNull(email) ||
+        !isEmail(email) || 
+        age < 18
+    ) {
+        return false;
+    }
+
+    accInfo.push(generateUniqueID(firstName, lastName))
+
+    try {
+        appendFileSync('users.txt', `${accInfo.join(',').toString()}\n`)
+        return true
+    } catch (error) {
+        console.error("Error appending data to file:", error.message);
+        return false
+    }
 }
